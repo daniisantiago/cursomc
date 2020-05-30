d5513f71140aed97d27d5ca1abf331a8e91dc122
Daniella Santiago <danii.santiago@gmail.com>
2020-05-30 18:04:45 -0300

    Atualizando Catrgoria com PUT

--- src/main/java/com/dani/cursomc/resources/ClienteResource.java
+++ src/main/java/com/dani/cursomc/resources/ClienteResource.java
@@ -18,8 +18,8 @@ public class ClienteResource {
 	private ClienteService service;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> findById(@PathVariable Integer id) {
-		Cliente obj = service.buscar(id);
+	public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
+		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
